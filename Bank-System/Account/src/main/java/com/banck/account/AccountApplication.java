53e4b2739ca4550aa125ffe1aa70521f872fc391
atul-patel-silver <atulpatel2001a@gmail.com>
2024-03-11 19:13:04 +0530

    commit

--- Bank-System/Account/src/main/java/com/banck/account/AccountApplication.java
+++ Bank-System/Account/src/main/java/com/banck/account/AccountApplication.java
@@ -1,5 +1,6 @@
 package com.banck.account;
 
+import com.banck.account.dto.AccountsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,6 +8,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -32,6 +34,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
                 url = "Hello"
         )
 )
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+
 public class AccountApplication {
 
     public static void main(String[] args) {
--- Bank-System/Account/src/main/java/com/banck/account/controller/AccountController.java
+++ Bank-System/Account/src/main/java/com/banck/account/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.banck.account.controller;
 
 import com.banck.account.constant.AccountConstant;
+import com.banck.account.dto.AccountsContactInfoDto;
 import com.banck.account.dto.CustomerDto;
 import com.banck.account.dto.ErrorResponseDto;
 import com.banck.account.dto.ResponseDto;
@@ -15,6 +16,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
@@ -25,7 +28,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/account", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
-@AllArgsConstructor
+
 
 @Tag(
         name = "CRUD REST APIs for Accounts in SBI",
@@ -34,8 +37,18 @@ import org.springframework.web.bind.annotation.*;
 public class AccountController {
 
 
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
+
     @Autowired
     private IAccountsService iAccountsService;
+
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
 
     /**
      * @param customerDto end point of create account of user
@@ -178,6 +191,87 @@ public class AccountController {
     }
 
 
+    @Operation(
+            summary = "Get Build information",
+            description = "Get Build information that is deployed into accounts microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(buildVersion);
+    }
+
+
+
+
+    @Operation(
+            summary = "Get Java version",
+            description = "Get Java versions details that is installed into accounts microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
+    }
+
+
+
+
+
+    @Operation(
+            summary = "Get Contact Info",
+            description = "Contact Info details that can be reached out in case of any issues"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfoDto> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(accountsContactInfoDto);
+    }
 
 
 
